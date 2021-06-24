9812b8d0ee98fb16069b249ea6982b5ad953cdb1
emc2thinker <tonylacson@gmail.com>
2021-06-23 20:30:47 -0700

    Lesson 2

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Tony Lacson',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 23, 2021'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 24, 2021'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 # Allows this file to be run with just 'py flaskblog.py' without setting environment variables and using 'flask run':
